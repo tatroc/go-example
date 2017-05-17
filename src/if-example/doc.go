6da14831e88244014d0f2d57a4ec364a04c85942
Chris Tatro <tatroc@gmail.com>
2017-05-16 19:36:59 -0500

    first commit

---
+++ src/if-example/doc.go
@@ -0,0 +1,6 @@
+// if-example project doc.go
+
+/*
+if-example document
+*/
+package main
---
+++ src/test/main.go
@@ -0,0 +1,12 @@
+// test project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x string = "Hello World"
+	fmt.Println(x)
+	fmt.Println(x)
+}
